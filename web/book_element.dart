3e5d688e1c341a15ebee2b621d813493b3a8c488
Carlos Martin Sanchez <carlosvin@gmail.com>
2015-11-15 01:42:35 +0100

    adding dart frontend

---
+++ web/book_element.dart
@@ -0,0 +1,12 @@
+@HtmlImport('book_element.html')
+library book_element;
+
+import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer/polymer.dart';
+
+@PolymerRegister('book-element')
+class BookElement extends PolymerElement {
+  @property String color = 'red';
+
+  BookElement.created() : super.created();
+}
---
+++ web/index.dart
@@ -0,0 +1,27 @@
+import 'package:polymer/polymer.dart';
+import 'book_element.dart';
+import 'dart:html';
+import 'dart:convert';
+
+/// Silence analyzer [BookElement]
+main() async {
+  loadData();
+  await initPolymer();
+}
+
+void loadData() {
+  var url = "http://localhost:8000/isbns";
+
+  // call the web server asynchronously
+  var request = HttpRequest.getString(url).then(onDataLoaded);
+}
+
+// print the raw json response text from the server
+void onDataLoaded(String responseText) {
+  var jsonString = responseText;
+  print(jsonString);
+  Map parsedMap = JSON.decode(jsonString);
+  BookElement book = querySelector('#book');
+
+  book.color = parsedMap['title'];
+}
